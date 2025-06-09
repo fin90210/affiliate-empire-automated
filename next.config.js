35b10dcaf8838fcb3f6e5d3bfda758bab68030fa
fin90210 <simpsonfin@gmail.com>
2025-06-09 21:49:28 +1000

    complete auto unzip

---
+++ next.config.js
@@ -0,0 +1,25 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
