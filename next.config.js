d41362a4f3b0aa894a86c97b7899f75b73a9c3ad
fin90210 <simpsonfin@gmail.com>
2025-06-09 22:08:42 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
-  images: {
-    domains: ['images.unsplash.com', 'via.placeholder.com'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
